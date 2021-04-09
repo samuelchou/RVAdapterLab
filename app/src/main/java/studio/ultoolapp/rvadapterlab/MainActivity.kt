f11d2407b5b6998d2e41d67635cb037424ed67a6
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-09 18:15:35 +0800

    feat(main/view): 現在可以在兩頁之間來回切換了。

--- app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
@@ -3,7 +3,12 @@ package studio.ultoolapp.rvadapterlab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import studio.ultoolapp.rvadapterlab.databinding.ActivityMainBinding
+import studio.ultoolapp.rvadapterlab.view.HeaderListFragment
+import studio.ultoolapp.rvadapterlab.view.SimpleListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        ViewPagerAdapter(supportFragmentManager).run {
+            addFragment(SimpleListFragment(), getString(R.string.tab_simple_list))
+            addFragment(HeaderListFragment(), getString(R.string.tab_header_list))
+            binding.viewPager.adapter = this
+        }
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+
+    internal class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        private val mFragmentList: MutableList<Fragment> = ArrayList()
+        private val mFragmentTitleList: MutableList<String> = ArrayList()
+        override fun getItem(position: Int): Fragment {
+            return mFragmentList[position]
+        }
+
+        override fun getCount(): Int {
+            return mFragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return mFragmentTitleList[position]
+        }
     }
 }
