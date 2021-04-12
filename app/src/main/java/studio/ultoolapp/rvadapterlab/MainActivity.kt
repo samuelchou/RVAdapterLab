55af96a66628593c69ea286c4c3a715a793cc80c
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-12 13:44:35 +0800

    fix(main): 現在首頁不再是 flexible 而是改為前往各地方的 Button list.

--- app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
@@ -5,10 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import studio.ultoolapp.rvadapterlab.databinding.ActivityMainBinding
-import studio.ultoolapp.rvadapterlab.view.HeaderListFragment
-import studio.ultoolapp.rvadapterlab.view.SimpleListFragment
-import studio.ultoolapp.rvadapterlab.view.component.ViewPagerAdapter
 import studio.ultoolapp.rvadapterlab.view.epoxy.EpoxyRootActivity
+import studio.ultoolapp.rvadapterlab.view.flexible.FlexibleRootActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,14 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        ViewPagerAdapter(supportFragmentManager).run {
-            addFragment(SimpleListFragment(), getString(R.string.tab_simple_list))
-            addFragment(HeaderListFragment(), getString(R.string.tab_header_list))
-            binding.viewPager.adapter = this
+        binding.btnOpenFlexible.setOnClickListener {
+            FlexibleRootActivity.getInitializeIntent(this).also { startActivity(it) }
         }
-        binding.tabLayout.setupWithViewPager(binding.viewPager)
 
-        binding.btnAirbnb.setOnClickListener {
+        binding.btnOpenEpoxy.setOnClickListener {
             Intent(this, EpoxyRootActivity::class.java).apply {
                 startActivity(this)
             }
