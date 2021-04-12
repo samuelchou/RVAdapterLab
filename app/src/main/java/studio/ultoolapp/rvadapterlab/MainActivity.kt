ed2905118a06562dff4844c936a47af5f3f979aa
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-12 10:07:13 +0800

    refactor(component/view): 將 ViewPagerAdapter 移動到外部，以便未來讓更多地方使用。

--- app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
@@ -3,12 +3,10 @@ package studio.ultoolapp.rvadapterlab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import studio.ultoolapp.rvadapterlab.databinding.ActivityMainBinding
 import studio.ultoolapp.rvadapterlab.view.HeaderListFragment
 import studio.ultoolapp.rvadapterlab.view.SimpleListFragment
+import studio.ultoolapp.rvadapterlab.view.component.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,26 +21,5 @@ class MainActivity : AppCompatActivity() {
             binding.viewPager.adapter = this
         }
         binding.tabLayout.setupWithViewPager(binding.viewPager)
-    }
-
-    internal class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        private val mFragmentList: MutableList<Fragment> = ArrayList()
-        private val mFragmentTitleList: MutableList<String> = ArrayList()
-        override fun getItem(position: Int): Fragment {
-            return mFragmentList[position]
-        }
-
-        override fun getCount(): Int {
-            return mFragmentList.size
-        }
-
-        fun addFragment(fragment: Fragment, title: String) {
-            mFragmentList.add(fragment)
-            mFragmentTitleList.add(title)
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return mFragmentTitleList[position]
-        }
     }
 }
