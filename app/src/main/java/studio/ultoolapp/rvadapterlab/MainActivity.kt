e02556ddfb1330aeb8449f761a6adaae61209ade
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-12 10:28:19 +0800

    feat(main/view): 提供按鈕。現在可以前往 Epoxy 相關頁面。

--- app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/MainActivity.kt
@@ -1,12 +1,14 @@
 package studio.ultoolapp.rvadapterlab
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import studio.ultoolapp.rvadapterlab.databinding.ActivityMainBinding
 import studio.ultoolapp.rvadapterlab.view.HeaderListFragment
 import studio.ultoolapp.rvadapterlab.view.SimpleListFragment
 import studio.ultoolapp.rvadapterlab.view.component.ViewPagerAdapter
+import studio.ultoolapp.rvadapterlab.view.epoxy.EpoxyRootActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,5 +23,11 @@ class MainActivity : AppCompatActivity() {
             binding.viewPager.adapter = this
         }
         binding.tabLayout.setupWithViewPager(binding.viewPager)
+
+        binding.btnAirbnb.setOnClickListener {
+            Intent(this, EpoxyRootActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
