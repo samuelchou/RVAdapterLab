3b0408e3e2ebe74b99455d0d7ed38287b0a65a57
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-14 16:42:22 +0800

    feat(epoxy-sticky): 調整介面，新增 Header 介面元件。

--- app/src/main/java/studio/ultoolapp/rvadapterlab/view/epoxy/HeaderEpoxyFragment.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/view/epoxy/HeaderEpoxyFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import studio.ultoolapp.rvadapterlab.databinding.FragmentHeaderEpoxyBinding
+import studio.ultoolapp.rvadapterlab.metadata.SimpleItem
 
 class HeaderEpoxyFragment : Fragment() {
     private lateinit var binding: FragmentHeaderEpoxyBinding
@@ -21,6 +22,19 @@ class HeaderEpoxyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.text = "Hello Epoxy Header"
+        val itemLists = getDummyLists(100)
+
+        SimpleEpoxyItemAdapter().apply {
+            binding.itemListRecycler.adapter = this
+            updateList(itemLists)
+        }
+    }
+
+    fun getDummyLists(amount: Int): List<SimpleItem> {
+        return mutableListOf<SimpleItem>().apply {
+            for (i in 0 until amount) {
+                add(SimpleItem("Title $i", "Subtitle $i"))
+            }
+        }
     }
 }
