585ae6323977ebdc30124fa564000546c737df6a
Samuel Chou <samuelchou@hotmail.com.tw>
2021-04-14 18:30:27 +0800

    feat(flexible-sticky): 初步架設，至少顯示清單。

--- app/src/main/java/studio/ultoolapp/rvadapterlab/view/flexible/HeaderFlexibleFragment.kt
+++ app/src/main/java/studio/ultoolapp/rvadapterlab/view/flexible/HeaderFlexibleFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import eu.davidea.flexibleadapter.FlexibleAdapter
 import studio.ultoolapp.rvadapterlab.databinding.FragmentHeaderListBinding
+import studio.ultoolapp.rvadapterlab.metadata.SimpleItem
 
 class HeaderFlexibleFragment : Fragment() {
 
@@ -23,6 +26,19 @@ class HeaderFlexibleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mDataList = getDummyItemList(30)
+        val mAdapter = FlexibleAdapter(mDataList.map { SimpleFlexibleItem(it) })
+
+        binding.itemListRecycler.apply {
+            layoutManager =
+                    LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = mAdapter
+        }
     }
 
+    private fun getDummyItemList(amount: Int): List<SimpleItem> =
+            mutableListOf<SimpleItem>().apply {
+                for (i in 0..amount) add(SimpleItem("Title $i", "Subtitle $i"))
+            }
 }
